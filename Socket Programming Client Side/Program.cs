0068d1c12bafd4383e93018b7c1db2d5c695ce78
camalzade_elvin <camalzade_elvin@outlook.com>
2019-07-05 20:50:15 +0400

    optimisation

--- Socket Programming Client Side/Program.cs
+++ Socket Programming Client Side/Program.cs
@@ -16,7 +16,7 @@ namespace Socket_Programming_Client_Side
             Name = Console.ReadLine();
             #region ClientMessage
             Console.WriteLine("======================CLIENT====================");
-            string ipAddress = "192.168.1.103"; // server ip
+            string ipAddress = "172.20.28.56"; // server ip
             int port = 1031;
             byte[] buffer = new byte[1024];
             IPEndPoint endp = new IPEndPoint(IPAddress.Parse(ipAddress), port);
@@ -56,67 +56,11 @@ namespace Socket_Programming_Client_Side
                 }
             });
             Task.WaitAll(senderTask, receiverTask);
-            //Parallel.Invoke(() => Send(socket), () => Receive(socket));
             #endregion
-            #region client
-
-            //byte[] buffer = new byte[256];
-            //IPEndPoint endp = new IPEndPoint(IPAddress.Parse("192.168.10.44"), 1031);
-            //Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            //sender.Connect(endp);
-
-            //if (sender.Connected) Console.WriteLine("connected");
-
-            //while (true)
-            //{
-            //    string message = Console.ReadLine();
-            //    buffer = Encoding.ASCII.GetBytes(message);
-            //    sender.Send(buffer);
-            //}
-
-            #endregion
-            #region Messaging with Server
-
-            //byte[] buffer = new byte[256];
-            //IPEndPoint endp = new IPEndPoint(IPAddress.Parse("192.168.10.44"), 1031);
-
-            //Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            //socket.Connect(endp);
-            //if (socket.Connected) Console.WriteLine("connected");
-
-
-            //while (true)
-            //{
-            //    string message = Console.ReadLine();
-            //    buffer = Encoding.ASCII.GetBytes(message);
-            //    socket.Send(buffer);
-            //    buffer = new byte[256];
-            //    int length = socket.Receive(buffer);
-            //    Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, length));
-            //}
-
-            #endregion
-            //IPHostEntry host = Dns.GetHostEntry("facebook.com");
+           
+          
         }
-        public static void Send(Socket socket)
-        {
-            while (true)
-            {
-                string message = Console.ReadLine();
-                socket.Send(Encoding.ASCII.GetBytes(message));
-                Console.WriteLine(Name + " - > " + message);
-            }
-        }
-        public static void Receive(Socket socket)
-        {
-            while (true)
-            {
-                byte[] buffer = new byte[256];
-                int length = socket.Receive(buffer);
-                Console.WriteLine("Server 1: " + Encoding.ASCII.GetString(buffer, 0, length));
-            }
-        }
+    
 
     }
 }
